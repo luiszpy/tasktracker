a6b604aa15507f6395c7c77e770e2fd526698092
luis <luiswty.o@gmail.com>
2026-05-04 15:28:55 -0300

    adding 'add weekly task'

--- main.py
+++ main.py
@@ -7,11 +7,10 @@
 
 
 
-choices = "[1]add task""\n""[2]update task""\n""[3]list task""\n""[4]delete taks"
+choices = "[1]add task""\n""[2]update task""\n""[3]list task""\n""[4]delete taks""\n"
 choice1 = int(input(choices))
 
 if choice1 == 1:#add task
-    task = str(input("what the task?"))
     frequency = int(input('''is:
 [1]daily
 [2]weekly
@@ -19,7 +18,28 @@ if choice1 == 1:#add task
 [4]annual
 [5]special
 '''))
+    tfreq = ""
+    if frequency == 1:
+        tfreq = "every day"
+    elif frequency == 2:
+        tfreq = "weekly"
+    elif frequency == 3:
+        tfreq = "monthly"
+    elif frequency == 4:
+        tfreq = "annual"
+    elif frequency == 5:
+        tfreq = "special"
+    
+
+    if frequency == 2:
+        wday = str(input("what week day?(1 monday to 7 sunday, ex: 1/2/7)""\n"))
+        
+
+
+    task = str(input("what the task?""\n"))
     time = str(input("time?"))
-    where = str(input)
+    where = str(input("where?"))
     with open(".tasks.txt", "a") as t:
-        t.write(f"{task} - {frequency} - {time} - {where}")
+        t.write(f"{task} - {tfreq} - {time} - {where}")
+            
+  
